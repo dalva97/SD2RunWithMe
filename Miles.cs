b772324b60feef4bbd67e04d3491aebb35101403
DulceAlvarado <candyalvarado998@yahoo.com>
2023-06-28 20:21:03 -0400

    revised

---
+++ Miles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RunWithMe
+{
+    public class Miles<T>
+    { 
+        public T data;
+
+        public Miles(T data)
+        {
+            this.data = data;
+            Console.WriteLine("Data passed: " + this.data);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        { 
+           Miles<int> milesran = new Miles<int>(23);
+        }
+    }
+}
+
+
