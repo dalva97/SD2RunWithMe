cf7fc89e493b9e5a9b48ebdc6da11be1c1197b17
DulceAlvarado <candyalvarado998@yahoo.com>
2023-07-23 15:24:29 -0400

    added another entity

---
+++ DatesDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RunWithMe;
+
+namespace RunWithMe
+{
+    public class DatesDBContext : DbContext
+    {
+        public DbSet<Dates> Datetime { get; set; }
+
+        public string DbPath { get; }
+
+        public DatesDBContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "DatesDB.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
+
